d8d57b070d4c853a3ad67c5786d9da04eafed80f
maksimovd7 <maksimovdanay@gmail.com>
2025-11-16 03:16:08 +0300

    LR1.java

---
+++ LR1.java
@@ -0,0 +1,11 @@
+//Список на Java
+ArrayList<String> list = new ArrayList<>();
+list.add("Москва");
+list.add("Воронеж");
+list.add("Новгород");
+
+//Cтек на Java
+MyStack<Integer> stack = new MyStack<>(10).
+stack.push(1);
+stack.push(2);
+stack.push(3);
